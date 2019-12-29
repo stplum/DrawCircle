b2d22286883e42e77d0c7607a36de0595d6c207c
umeda tohru <umeda@sun.cims.jp>
2019-12-29 20:12:42 +0900

    カスタムビューの追加

--- app/src/main/java/com/example/drawcircle/MainActivity.kt
+++ app/src/main/java/com/example/drawcircle/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.drawcircle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        if (event != null) {
+            myView.drawColorfulCircle(event.x, event.y)
+            myView.invalidate()
+        }
+        return super.onTouchEvent(event)
     }
 }
---
+++ app/src/main/java/com/example/drawcircle/MyView.kt
@@ -0,0 +1,33 @@
+package com.example.drawcircle
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+
+class MyView(context: Context, attributeSet: AttributeSet): View(context, attributeSet) {
+    private val paint: Paint = Paint()
+    private val backingBitmap: Bitmap = Bitmap.createBitmap(1080, 2060, Bitmap.Config.ARGB_8888)
+    private val offsetY: Float = 240f                       // Status Bar + Title Bar (72+168)
+    private var strokeWidth: Float = 10f
+
+    init {
+        paint.style = Paint.Style.STROKE
+        paint.isAntiAlias = true
+        paint.strokeWidth = strokeWidth
+        paint.color = Color.argb(255, 255, 0, 0)
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        canvas?.drawBitmap(backingBitmap, 0f, 0f, paint)
+    }
+
+    fun drawColorfulCircle(cx: Float, cy: Float) {
+        val canvas = Canvas(backingBitmap)
+        canvas.drawCircle(cx, cy - offsetY, 30f, paint)
+    }
+}
