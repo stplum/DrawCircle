e2678e13afbc08610492a0369cdf452a641e75fe
umeda tohru <umeda@sun.cims.jp>
2019-12-29 23:13:45 +0900

    ボタン効果音の追加

--- app/src/main/java/com/example/drawcircle/MainActivity.kt
+++ app/src/main/java/com/example/drawcircle/MainActivity.kt
@@ -1,22 +1,40 @@
 package com.example.drawcircle
 
+import android.media.AudioAttributes
+import android.media.SoundPool
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MotionEvent
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var soundPool: SoundPool
+    private var soundSelect: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // ボタン押下処理の登録
         minusButton.setOnClickListener {
             myView.changeRadius(0)
+            playSE(soundSelect)
         }
         plusButton.setOnClickListener {
             myView.changeRadius(1)
+            playSE(soundSelect)
         }
+
+        // SE音の準備
+        val audioAttributes = AudioAttributes.Builder()
+            .setUsage(AudioAttributes.USAGE_GAME)
+            .setContentType(AudioAttributes.CONTENT_TYPE_SPEECH)
+            .build()
+        soundPool = SoundPool.Builder()
+            .setAudioAttributes(audioAttributes)
+            .setMaxStreams(1)
+            .build()
+        soundSelect = soundPool.load(this, R.raw.select01, 1)
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
@@ -32,5 +50,9 @@ class MainActivity : AppCompatActivity() {
             }
         }
         return super.onTouchEvent(event)
+    }
+
+    private fun playSE(sound: Int) {
+        soundPool.play(sound, 1f, 1f, 0, 0, 1f)
     }
 }
